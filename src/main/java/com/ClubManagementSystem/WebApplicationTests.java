3b7f90516d98421045d09461b5f722bf0d3c97b3
dianeuwase <uwasedianne8@gmail.com>
2023-12-07 14:29:18 +0200

    final commmit

---
+++ src/main/java/com/ClubManagementSystem/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClubManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
